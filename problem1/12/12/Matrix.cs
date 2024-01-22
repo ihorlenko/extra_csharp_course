467a361fc991d22da239a2645885871f52a353bc
Ihor Orlenko <eaglyig@gmail.com>
2024-01-23 00:09:01 +0200

    Solved problem 1-12

---
+++ problem1/12/12/Matrix.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace MatrixNS
+{
+	public struct Matrix
+	{
+		private int[,] data;
+		public int size { get; private set; }
+
+        public Matrix(int size)
+        {
+            this.size = size;
+            data = new int[size, size];
+        }
+
+        public void FillWithRandom()
+        {
+            Random rand = new Random();
+
+            for (int i = 0; i < size; ++i)
+            {
+                for (int j = 0; j < size; ++j)
+                {
+                    data[i, j] = rand.Next(-100, 100);
+                }
+            }
+        }
+
+        public int[] CalcDiagonalSums()
+        {
+            int[] diagonalSums = new int[2 * size - 1];
+            for (int diagIndex = -size + 1; diagIndex < size; ++diagIndex)
+            {
+                for (int i = 0; i < size; ++i)
+                {
+                    int j = diagIndex + i;
+                    if (j >= 0 && j < size)
+                    {
+                        diagonalSums[diagIndex + size - 1] += data[i, j];
+                    }
+                }
+            }
+
+            return diagonalSums;
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < size; ++i)
+            {
+                for (int j = 0; j < size; ++j)
+                {
+                    Console.Write($"{data[i, j], 4}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
+}
+
---
+++ problem1/12/12/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using MatrixNS;
+
+public class Program
+{
+    public static void Main()
+    {
+        int size = 5;
+
+        Matrix matrix = new Matrix(size);
+        matrix.FillWithRandom();
+
+        Console.WriteLine($"Randomly generated {size}*{size} matrix:");
+        matrix.Print();
+
+        int[] diagonalSums = matrix.CalcDiagonalSums();
+        Console.WriteLine();
+        Console.WriteLine($"Diagonal sums calculated for this matrix:");
+        foreach (int sum in diagonalSums)
+        {
+            Console.Write($"{sum, 6}");
+        }
+        Console.WriteLine();
+    }
+}
