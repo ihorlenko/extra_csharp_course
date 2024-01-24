3f03f87cbfa62f0f10e3f732c6c825d336efc0c1
Ihor Orlenko <eaglyig@gmail.com>
2024-01-24 17:17:10 +0200

    Solved 4-12

---
+++ problem4/12/12/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static Dictionary<int, double> memo = new Dictionary<int, double>();
+
+    public static void Main()
+    {
+        Console.WriteLine($"{Polynom(100, 10):F2}");
+    }
+
+    public static double Polynom(int m, int x)
+    {
+        if (m < 0)
+        {
+            throw new ArgumentException("m must be non-negative");
+        }
+        if (m == 0) { return 1.0; }
+        if (m == 1) { return (double)x; }
+
+        if (memo.ContainsKey(m))
+            return memo[m];
+
+        memo[m] = (3.0 * m - 1) / m * Polynom(m - 1, x) - x * Polynom(m - 2, x);
+        return memo[m];
+    }
+}
